87929c6ee282987ee6695e4e1d4d3453589de4e9
SK <v.buh911@gmail.com>
2024-12-21 10:54:33 +0200

    doit

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,22 @@
-function getElementWidth(content, padding, border) {
-    content=Number.parseFloat(content);
-    padding=Number.parseFloat(padding);
-    border=Number.parseFloat(border);
+const profile = {
+    username: "Jacob",
+    playTime: 300,
     
-    const boxSizing=content + padding*2 + border*2;
-    return boxSizing;
-}
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
